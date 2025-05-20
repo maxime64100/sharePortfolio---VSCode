bf991271a4c6304eddcbe9d33d53403ab2529a84
maxime64100 <maximefallek1@gmail.com>
2025-05-20 15:17:00 +0200

    ajout de la classe portefeuille + son test + tentative de debug pipeline

---
+++ src/main/java/fr/utc/miage/shares/Portefeuille.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.utc.miage.shares;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Portefeuille {
+    private Map<Action, Integer> actions;
+
+    public Portefeuille() {
+        actions = new HashMap<>();
+    }
+
+    public void acheter(Action action, int quantite) {
+        if (quantite <= 0) {
+            throw new IllegalArgumentException("La quantité doit être positive.");
+        }
+        actions.put(action, actions.getOrDefault(action, 0) + quantite);
+    }
+
+    public void vendre(Action action, int quantite) {
+        if (!actions.containsKey(action)) {
+            throw new IllegalArgumentException("Action non détenue dans le portefeuille.");
+        }
+        int quantiteActuelle = actions.get(action);
+        if (quantite > quantiteActuelle) {
+            throw new IllegalArgumentException("Quantité à vendre supérieure à la quantité détenue.");
+        }
+        if (quantite == quantiteActuelle) {
+            actions.remove(action);
+        } else {
+            actions.put(action, quantiteActuelle - quantite);
+        }
+    }
+
+    public double valeur(Jour jour) {
+        double valeurTotale = 0.0;
+        for (Map.Entry<Action, Integer> entry : actions.entrySet()) {
+            Action action = entry.getKey();
+            int quantite = entry.getValue();
+            valeurTotale += quantite * action.valeur(jour);
+        }
+        return valeurTotale;
+    }
+
+    public Map<Action, Integer> getActions() {
+        return new HashMap<>(actions);
+    }
+
+    public int getQuantite(Action action) {
+        return actions.getOrDefault(action, 0);
+    }
+}
