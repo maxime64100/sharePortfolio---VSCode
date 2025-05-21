d25f554ab711a3db601a3642ca6f2d3f7a8e4944
Mathis-Lague <laguemathis@gmail.com>
2025-05-21 12:09:05 +0200

    ajout license

--- src/test/java/fr/utc/miage/shares/PortefeuilleTest.java
+++ src/test/java/fr/utc/miage/shares/PortefeuilleTest.java
@@ -1,5 +1,19 @@
 package fr.utc.miage.shares;
-
+/*
+ * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import fr.utc.miage.shares.Action;
 import fr.utc.miage.shares.ActionSimple;
 import fr.utc.miage.shares.ActionComposee;
@@ -73,7 +87,7 @@ public class PortefeuilleTest {
         assert affichage.contains("Quantité : 10");
         assert affichage.contains("Quantité : 5");
     }
-    
+
     @Test
     void testAcheter() {
 
