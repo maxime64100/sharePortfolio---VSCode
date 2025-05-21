aa3535e85204cf7ffc8b3ad69da6ed50b6b6eefa
Mathis-Lague <laguemathis@gmail.com>
2025-05-21 11:52:29 +0200

    ajout de commentaires

--- src/test/java/fr/utc/miage/shares/PortefeuilleTest.java
+++ src/test/java/fr/utc/miage/shares/PortefeuilleTest.java
@@ -14,12 +14,16 @@ public class PortefeuilleTest {
     //test d'affichage des actions
     @Test
     void testConsulterPortefeuilleActionSimple() {
+        //Arrange
         Portefeuille portefeuille = new Portefeuille();
         Action action1 = new ActionSimple("GOOGLE");
         Action action2 = new ActionSimple("FACEBOOK");
+
+        //Action
         portefeuille.acheter(action1, 10);
         portefeuille.acheter(action2, 5);
 
+        //Assert
         assert portefeuille.getActions().size() == 2;
         assert portefeuille.getQuantite(action1) == 10;
         assert portefeuille.getQuantite(action2) == 5;
@@ -28,47 +32,48 @@ public class PortefeuilleTest {
 
     @Test
     void testConsulterPortefeuilleActionComposee() {
+        //Arrange
         ActionSimple facebookStock = new ActionSimple("FB1");
         ActionSimple metaStock = new ActionSimple("META1");
-
+        Portefeuille portefeuille = new Portefeuille();
         Map<ActionSimple, Float> proportions = new HashMap<>();
+
+        //Action
         proportions.put(facebookStock, 0.6f);
         proportions.put(metaStock, 0.4f);
-
-        Portefeuille portefeuille = new Portefeuille();
         Action actionComposee = new ActionComposee("FACEBOOK", proportions);
-
         portefeuille.acheter(actionComposee, 10);
         portefeuille.acheter(actionComposee, 5);
 
+        //Assert
         assert portefeuille.getActions().containsKey(actionComposee);
         assert portefeuille.getQuantite(actionComposee) == 15;
     }
 
     @Test
-void testAfficherDetailsPortefeuille() {
-    ActionSimple google = new ActionSimple("GOOGLE");
-    ActionSimple fb1 = new ActionSimple("FB1");
+    void testAfficherDetailsPortefeuille() {
+        //Arrange
+        ActionSimple google = new ActionSimple("GOOGLE");
+        ActionSimple fb1 = new ActionSimple("FB1");
+        Map<ActionSimple, Float> proportions = new HashMap<>();
+        Portefeuille portefeuille = new Portefeuille();
 
-    Map<ActionSimple, Float> proportions = new HashMap<>();
-    proportions.put(fb1, 0.6f);
-    proportions.put(google, 0.4f);
+        //Action
+        proportions.put(fb1, 0.6f);
+        proportions.put(google, 0.4f);
+        ActionComposee composedFb = new ActionComposee("FACEBOOK", proportions);
+        portefeuille.acheter(google, 10);
+        portefeuille.acheter(composedFb, 5);
+        String affichage = portefeuille.afficherPortefeuille();
+        System.out.println(affichage); // utile pour visualiser pendant le dev
 
-    ActionComposee composedFb = new ActionComposee("FACEBOOK", proportions);
-
-    Portefeuille portefeuille = new Portefeuille();
-    portefeuille.acheter(google, 10);
-    portefeuille.acheter(composedFb, 5);
-
-    String affichage = portefeuille.afficherPortefeuille();
-
-    System.out.println(affichage); // utile pour visualiser pendant le dev
-
-    assert affichage.contains("GOOGLE");
-    assert affichage.contains("FACEBOOK");
-    assert affichage.contains("Quantité : 10");
-    assert affichage.contains("Quantité : 5");
-}
+        //Assert
+        assert affichage.contains("GOOGLE");
+        assert affichage.contains("FACEBOOK");
+        assert affichage.contains("Quantité : 10");
+        assert affichage.contains("Quantité : 5");
+    }
+    
     @Test
     void testAcheter() {
 
