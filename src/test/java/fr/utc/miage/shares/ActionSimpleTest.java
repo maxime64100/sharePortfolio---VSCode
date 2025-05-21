8f37c497a38a4c9a094a0b8fab963fa7e884b24f
tom montbord <montbordino@gmail.com>
2025-05-21 10:22:05 +0200

    Ajout de la licence

--- src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
+++ src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright 2025 David Navarre &lt;David.Navarre at irit.fr&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package fr.utc.miage.shares;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ActionSimpleTest {
     private static final String FOO_SHARE1 = "Foo Share 1";
@@ -16,9 +29,9 @@ class ActionSimpleTest {
 
     private static ActionSimple actionSimple1;
     private static ActionSimple actionSimple2;
-    private static Jour jour1 = new Jour(CURRENT_YEAR, CURRENT_DAY);
-    private static Jour jour2 = new Jour(CURRENT_YEAR, CURRENT_DAY+1);
-    private static Jour jour3 = new Jour(CURRENT_YEAR, CURRENT_DAY+2);
+    private static final Jour jour1 = new Jour(CURRENT_YEAR, CURRENT_DAY);
+    private static final Jour jour2 = new Jour(CURRENT_YEAR, CURRENT_DAY+1);
+    private static final Jour jour3 = new Jour(CURRENT_YEAR, CURRENT_DAY+2);
 
 
     private void setupActions() {
