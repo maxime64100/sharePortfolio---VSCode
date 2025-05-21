57da085819b122ddfb543e150e6771de683d826f
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2025-05-21 10:28:36 +0200

    test(ActionSimple): change coms

--- src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
+++ src/test/java/fr/utc/miage/shares/ActionSimpleTest.java
@@ -10,29 +10,29 @@ class ActionSimpleTest {
 
     @Test
     void valeurActionSimpleDatePrecise() {
-        // Enregistrement du cours de l'action pour une date précise
+        // Arrange
         action.enrgCours(new Jour(2023, 122), 150.0f);
 
-        // Vérification de la valeur de l'action pour cette date
+        // Assert
         assertEquals(150.0f, action.valeur(new Jour(2023, 122)));
     }
 
     @Test
     void valeurActionSimpleJourFerieMarcheFerme() {
-        // Enregistrement du cours de l'action pour une date précise
+        // Arrange
         action.enrgCours(new Jour(2024, 366), 200.0f); // December 31, 2024 (leap year)
         action.enrgCours(new Jour(2025, 1), 0);   // January 1, 2025
 
-        // Vérification de la valeur de l'action pour un jour férié (marché fermé)
+        // Assert
         assertEquals(200.0f, action.valeur(new Jour(2025, 1)));
     }
 
     @Test
     void dateFutureSansValeurDisponible() {
-        // Enregistrement du cours de l'action pour une date précise
+        // Arrange
         action.enrgCours(new Jour(2023, 122), 150.0f);
 
-        // Vérification de la valeur de l'action pour une date future sans valeur disponible
+        // Assert
         assertEquals(0.0f, action.valeur(new Jour(2024, 1)));
     }
 
