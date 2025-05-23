55933aca8b52c09ef024b041ff02e2cdde61c44b
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2025-05-23 10:27:27 +0200

    feat(Marche): creation objet Marche

---
+++ src/main/java/fr/utc/miage/shares/Marche.java
@@ -0,0 +1,45 @@
+package fr.utc.miage.shares;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Marche {
+
+    private String id; // UUID ou code unique
+    private String nom; // ex : "Euronext"
+    private String pays; // ex : "France"
+    private String devise; // ex : "EUR"
+    private ZoneId fuseauHoraire; // ex : ZoneId.of("Europe/Paris")
+    private LocalTime heureOuverture;
+    private LocalTime heureFermeture;
+    private Set<Action> actions; // ou List si ordre important
+
+
+    public boolean estOuvert(LocalTime maintenant) {
+        return !maintenant.isBefore(heureOuverture) && !maintenant.isAfter(heureFermeture);
+    }
+
+    public void ajouterAction(Action action) {
+        actions.add(action);
+    }
+
+    public void retirerAction(Action action) {
+        actions.remove(action);
+    }
+
+    public List<Action> listerActionsParNom() {
+        return actions.stream()
+                .sorted(Comparator.comparing(Action::getLibelle))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return nom + " (" + pays + ", " + devise + ")";
+    }
+
+}
